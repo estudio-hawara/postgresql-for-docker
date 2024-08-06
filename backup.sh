55c83a9afec7f887a70cf45afc24d38b4e91b90b
Carlos Capote Pérez-Andreu <carlos.capote@hawara.es>
2024-08-06 22:37:15 +0100

    add a db suffix to backups

--- backup.sh
+++ backup.sh
@@ -7,10 +7,10 @@ DATABASE=$1
 if [ -z "$DATABASE" ]
 then
     echo "The database name is missing. Try again:"
-    echo "> backup-database.sh <database>"
+    echo "> backup.sh <database>"
     exit 1
 fi
 
 ./dc exec postgresql dump-database.sh $DATABASE > $BACKUPS_FOLDER/$DATABASE.sql
 
-tar -zcvf $BACKUPS_FOLDER/$DATABASE.tar.gz --remove-files --absolute-names $BACKUPS_FOLDER/$DATABASE.sql
+tar -zcvf $BACKUPS_FOLDER/$DATABASE-db.tar.gz --remove-files --absolute-names $BACKUPS_FOLDER/$DATABASE.sql
